60ba09e2f79e65f6469435a688ab0266c34df898
Mohit Rajput <mohitrajput987@gmail.com>
2022-02-12 13:12:16 +0530

    feat: setup splash screen theme

--- app/src/main/java/com/otb/splashdemo/MainActivity.kt
+++ app/src/main/java/com/otb/splashdemo/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.otb.splashdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
+        setupPreDrawListener()
+    }
+
+    /**
+     * Use this method only if you want to keep splash screen for longer than cold start time.
+     * Manage `isReady` flag according to your logic.
+     */
+    private fun setupPreDrawListener() {
+        // Set up an OnPreDrawListener to the root view.
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // Check if the initial data is ready.
+                    val isReady = true
+                    return if (isReady) {
+                        // The content is ready; start drawing.
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        // The content is not ready; suspend.
+                        false
+                    }
+                }
+            }
+        )
     }
 }
